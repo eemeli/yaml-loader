887f08b4ae81ed244f66bc6fe009158f2d1d64ec
Joseph Nudell <joenudell@gmail.com>
2015-06-19 10:55:34 -0500

    Add option to suppress js-yaml warnings

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var yaml = require('js-yaml');
+var loaderUtils = require('loader-utils');
+var assign = require('object-assign');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
-  var res = yaml.safeLoad(source);
+
+  var opts = assign({
+    force: false
+  }, loaderUtils.parseQuery(this.query));
+
+  var jsYamlOpts = {
+    onWarning: opts.force ? function() {} : null
+  };
+
+  var res = yaml.safeLoad(source, jsYamlOpts);
   return JSON.stringify(res, undefined, '\t');
 };
