72722b95745f03dfd6b59690dc0da23638b8846d
Joe Fiorini <joe@joefiorini.com>
2016-05-26 19:49:33 -0400

    Use webpack loader options to pass to js-yaml

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var yaml = require('js-yaml');
 
 module.exports = function (source) {
   this.cacheable && this.cacheable();
-  var res = yaml.safeLoad(source);
+  var options = this.options.yaml;
+  options.filename = this.resourcePath;
+  var res = yaml.load(source, options);
   return JSON.stringify(res, undefined, '\t');
 };
