8145fb45a41f200e47aabd222fb93439168eee0c
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-07-14 20:36:39 +0200

    Revert "Return JSON as string. yaml-loader should be chained with json-loader."
    This reverts commit b05cba0dc659e1c60283224cdf558bc085061db7.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var yaml = require('js-yaml');
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var res = yaml.safeLoad(source);
-  return JSON.stringify(res, undefined, '\t');
+  return 'module.exports = ' + JSON.stringify(res, undefined, '\t');
 };
