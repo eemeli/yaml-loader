3beb5552a95789591270e289a7616f8360aa137c
Luis G <luis.gomes@commercetools.de>
2019-05-20 10:21:26 +0200

    changes `safeLoad` to `safeLoadAll`
     so it can load a file with multiple YAML documents

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var yaml = require('js-yaml');
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   try {
-    var res = yaml.safeLoad(source);
+    var res = yaml.safeLoadAll(source);
     return JSON.stringify(res, undefined, '\t');
   }
   catch (err) {
