7fa1dd43f7d8d32d16eab75767a991c4d55eb43f
Eemeli Aro <eemeli@gmail.com>
2020-03-15 21:29:39 +0200

    feat: Add asJSON option
    BREAKING CHANGE: This changes the default output from stringified JSON
    to stringified JS.

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ var loaderUtils = require('loader-utils')
 var YAML = require('yaml')
 
 module.exports = function yamlLoader(src) {
-  const { asStream, namespace, ...options } = Object.assign(
+  const { asJSON, asStream, namespace, ...options } = Object.assign(
     { prettyErrors: true },
     loaderUtils.getOptions(this)
   )
 
+  let res
   if (asStream) {
     const stream = YAML.parseAllDocuments(src, options)
-    const res = []
+    res = []
     for (const doc of stream) {
       for (const warn of doc.warnings) this.emitWarning(warn)
       for (const err of doc.errors) throw err
       res.push(doc.toJSON())
     }
-    return JSON.stringify(res)
+  } else {
+    res = YAML.parse(src, options)
+    if (namespace) {
+      res = namespace.split('.').reduce(function(acc, name) {
+        return acc[name]
+      }, res)
+    }
   }
 
-  let res = YAML.parse(src, options)
-  if (namespace) {
-    res = namespace.split('.').reduce(function(acc, name) {
-      return acc[name]
-    }, res)
-  }
-  return JSON.stringify(res)
+  const json = JSON.stringify(res)
+  return asJSON ? json : `export default ${json};`
 }
--- test/test.js
+++ test/test.js
@@ -1,43 +1,51 @@
 const loader = require('../')
 
-test('return stringify version of the yaml file', () => {
-  const res = loader('---\nhello: world')
-  expect(res).toBe('{"hello":"world"}')
+describe('options.asJSON', () => {
+  test('return stringify version of the yaml file', () => {
+    const ctx = { query: { asJSON: true } }
+    const src = '---\nhello: world'
+    const res = loader.call(ctx, src)
+    expect(res).toBe('{"hello":"world"}')
+  })
+
+  test('throw error if there is a parse error', () => {
+    const ctx = { query: { asJSON: true } }
+    const src = '---\nhello: world\nhello: 2'
+    expect(() => loader.call(ctx, src)).toThrow(
+      /^Map keys must be unique; "hello" is repeated/
+    )
+  })
 })
 
-test('throw error if there is a parse error', () => {
-  let msg = null
-  try {
-    loader('---\nhello: world\nhello: 2')
-  } catch (error) {
-    msg = error.message
-  }
-  expect(msg).toMatch(/^Map keys must be unique; "hello" is repeated/)
+describe('options.namespace', () => {
+  test('return a part of the yaml', () => {
+    const ctx = { query: '?namespace=hello' }
+    const res = loader.call(ctx, '---\nhello:\n  world: plop')
+    expect(res).toBe('export default {"world":"plop"};')
+  })
+
+  test('return a sub-part of the yaml', () => {
+    const ctx = { query: '?namespace=hello.world' }
+    const res = loader.call(ctx, '---\nhello:\n  world: plop')
+    expect(res).toBe('export default "plop";')
+  })
 })
 
-test('return a part of the yaml', () => {
-  const ctx = { query: '?namespace=hello' }
-  const res = loader.call(ctx, '---\nhello:\n  world: plop')
-  expect(res).toBe('{"world":"plop"}')
-})
+describe('options.asStream', () => {
+  test('with asStream, parse multiple documents', () => {
+    const ctx = { query: { asStream: true } }
+    const src = 'hello: world\n---\nsecond: document\n'
+    const res = loader.call(ctx, src)
+    expect(res).toBe(
+      'export default [{"hello":"world"},{"second":"document"}];'
+    )
+  })
 
-test('return a sub-part of the yaml', () => {
-  const ctx = { query: '?namespace=hello.world' }
-  const res = loader.call(ctx, '---\nhello:\n  world: plop')
-  expect(res).toBe('"plop"')
-})
-
-test('with asStream, parse multiple documents', () => {
-  const ctx = { query: { asStream: true } }
-  const src = 'hello: world\n---\nsecond: document\n'
-  const res = loader.call(ctx, src)
-  expect(res).toBe('[{"hello":"world"},{"second":"document"}]')
-})
-
-test('without asStream, fail to parse multiple documents', () => {
-  const ctx = { query: { asStream: false } }
-  const src = 'hello: world\n---\nsecond: document\n'
-  expect(() => loader.call(ctx, src)).toThrow(
-    /^Source contains multiple documents/
-  )
+  test('without asStream, fail to parse multiple documents', () => {
+    const ctx = { query: { asStream: false } }
+    const src = 'hello: world\n---\nsecond: document\n'
+    expect(() => loader.call(ctx, src)).toThrow(
+      /^Source contains multiple documents/
+    )
+  })
 })
